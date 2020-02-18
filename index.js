0c751cffbd7155817df1cac2bd936cbcec570786
Christopher Floyd <cfloyd252@gmail.com>
2020-02-18 10:46:09 -0700

    working on 2

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ app.get('/sum', (req, res) => {
   res.send(`The sum of ${a} and ${b} is ${c}`);
 });
 
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = Number(req.query.shift);
+  const convertedText = Array.from(text).map(index => {
+    index.
+  })
+
+  res.send(Array.from(text));
+});
+
 app.listen(8080, () => console.log('Server on 8080'));
